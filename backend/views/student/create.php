fe359dedd3079e64ceb86085dd055e0ee2616688
Pratham  Srivastava <pratham@nuwaveindia.net>
2024-09-30 16:33:29 +0530

    V6

--- backend/views/student/create.php
+++ backend/views/student/create.php
@@ -10,7 +10,7 @@ $this->params['breadcrumbs'][] = ['label' => 'Students', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="student-create">
-    <h1>Hello Pratham V5</h1>
+    <h1>Hello Pratham V6</h1>
 <h1> <?=Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
